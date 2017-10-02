5f5062ac5789e25fca28b0567289f84d6eee9708
Paul Franco <paulfranco@me.com>
2017-10-01 22:57:20 -0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Fundamentals_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Loop that prints 1 to 100
+            for (int num = 1; num <= 100; num++)
+            {
+                if(!(num % 15 == 0))
+                {
+                    if(num % 3 == 0 || num % 5 == 0)
+                    {
+                        Console.WriteLine(num);
+                    }
+                }
+                
+            }
+            ///fizz buzz
+            for (int num = 1; num < 101; num++)
+            {
+                if(num % 3 == 0 && num % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (num % 5 == 0)
+                {
+                   Console.WriteLine("Buzz");
+                }
+            }
+        }
+    }
+}
